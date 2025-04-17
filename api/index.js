8c414c6bedd245982fa3949b991d20f338e4ebbe
AYD-7 <ayodejiaronimo@gmail.com>
2025-04-17 13:43:22 +0100

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("MongoDB is connected");
+        
+    }).catch((err) => {
+        console.log(err);
+        
+    })
+
+// Creating the server
 const app = express();
 
+// Port where the server is going to run
 app.listen(3000, () => {
-    console.log("Server is now running at port 3000!");
+    console.log("Server is now running at port 3000! You are good to go!! Thank you for using our platform!!!");
     
 });
+
